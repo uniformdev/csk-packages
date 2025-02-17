e278b6e8c086a14523b9b9e7cd55e2f2399b534b
trsergey <stu@dbbsoftware.com>
2025-02-17 12:12:11 +0200

    chore: scripts improvements

--- apps/csk-v-next/src/app/[[...path]]/page.tsx
+++ apps/csk-v-next/src/app/[[...path]]/page.tsx
@@ -1,10 +1,4 @@
-import { cookies } from 'next/headers';
-import {
-  createServerUniformContext,
-  ContextUpdateTransfer,
-  PageParameters,
-  UniformComposition,
-} from '@uniformdev/canvas-next-rsc';
+import { PageParameters, UniformComposition } from '@uniformdev/canvas-next-rsc';
 import { emptyPlaceholderResolver } from '@uniformdev/csk-components/components/canvas/emptyPlaceholders';
 import { DesignExtensionsProvider } from '@uniformdev/design-extensions-tools/components/providers/server';
 import { componentResolver } from '@/components';
@@ -13,30 +7,14 @@ import retrieveRoute from '@/utils/retrieveRoute';
 
 export default async function Home(props: PageParameters) {
   const route = await retrieveRoute(props, locales.defaultLocale);
-
-  const cookie = await cookies();
-  const theme = cookie.get('theme')?.value || 'light';
   const searchParams = await props.searchParams;
-  const serverContext = await createServerUniformContext({
-    searchParams,
-  });
   const isPreviewMode = searchParams?.is_incontext_editing_mode === 'true';
-
   return (
     <DesignExtensionsProvider isPreviewMode={isPreviewMode}>
-      <ContextUpdateTransfer
-        serverContext={serverContext}
-        update={{
-          quirks: {
-            theme,
-          },
-        }}
-      />
       <UniformComposition
         {...props}
         route={route}
         resolveComponent={componentResolver}
-        serverContext={serverContext}
         mode="server"
         resolveEmptyPlaceholder={emptyPlaceholderResolver}
       />
--- apps/csk-v-next/src/app/layout.tsx
+++ apps/csk-v-next/src/app/layout.tsx
@@ -7,14 +7,13 @@ import '@/styles/dimensions.css';
 import '@/styles/fonts.css';
 import '@/styles/borders.css';
 import { customFontVariables } from '@/fonts';
-import { UniformClientContext } from '@/utils/clientContext';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={customFontVariables}>
         <NextThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <UniformContext clientContextComponent={UniformClientContext}>{children}</UniformContext>
+          <UniformContext>{children}</UniformContext>
         </NextThemeProvider>
       </body>
     </html>
