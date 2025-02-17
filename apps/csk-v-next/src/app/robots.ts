d321a4b5146ce8f1feb03835b11f1d751a1229a2
eugeneshulga869 <82021957+eugeneshulga869@users.noreply.github.com>
2025-02-17 17:15:44 +0200

    feat: use BASE_URL instead of VERCEL_URL, remove apiHost(#61)

--- apps/csk-v-next/src/app/robots.ts
+++ apps/csk-v-next/src/app/robots.ts
@@ -1,9 +1,9 @@
 import type { MetadataRoute } from 'next';
 
-const VERCEL_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : '';
+const BASE_URL = process.env.BASE_URL ? `https://${process.env.BASE_URL}` : '';
 
 export default function robots(): MetadataRoute.Robots {
-  const domain = VERCEL_URL || 'http://localhost:3000';
+  const domain = BASE_URL || 'http://localhost:3000';
   return {
     rules: {
       userAgent: '*',
