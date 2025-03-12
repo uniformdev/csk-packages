76f8eda18eb96cafc8f398a32a593b3ab93002e1
Alex Shyba <alex.shyba@uniform.dev>
2025-03-11 20:32:13 -0700

    feat: template applied

--- apps/csk-v-next/src/components/custom-canvas/Container.tsx
+++ apps/csk-v-next/src/components/custom-canvas/Container.tsx
@@ -3,8 +3,16 @@ import {
   Container as CSKContainer,
   ContainerProps as CSKContainerProps,
 } from '@uniformdev/csk-components/components/canvas';
+import { cn } from '@uniformdev/csk-components/utils/styling';
+
+type ContainerProps = CSKContainerProps & {
+  fitContent?: boolean;
+  relative?: boolean;
+};
 
 // This is an example of how you can override an existing CSK component based on the Container component.
-const Container: FC<CSKContainerProps> = props => <CSKContainer {...props} />;
+const Container: FC<ContainerProps> = ({ fitContent, relative, ...props }) => (
+  <CSKContainer {...props} className={cn({ 'w-fit h-fit': fitContent, relative: relative })} />
+);
 
 export default Container;
