fe12c345851ef80f8df10e5b85fefffe6636fcdf
Michael Lonin <michael.lonin@gmail.com>
2025-04-04 13:38:44 +0300

    fix: rich text, always keep full container width

--- apps/csk-v-next/src/components/custom-canvas/RichText.tsx
+++ apps/csk-v-next/src/components/custom-canvas/RichText.tsx
@@ -5,7 +5,7 @@ import {
 } from '@uniformdev/csk-components/components/canvas';
 
 const RichText: FC<CSKRichTextProps> = props => (
-  <CSKRichText className="w-full md:w-3/4 [&>table]:ml-2 [&>table]:text-base [&_td>p]:!m-0 [&_td>p]:p-2" {...props} />
+  <CSKRichText className="w-full [&>table]:ml-2 [&>table]:text-base [&_td>p]:!m-0 [&_td>p]:p-2" {...props} />
 );
 
 export default RichText;
