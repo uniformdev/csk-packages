bf3b369050b313011b11eb8f43b0a19c1a093a7a
Eugene <eugene.shulga@dbbsoftware.com>
2025-04-04 11:24:12 +0300

    feat: add loading skeleton

--- apps/csk-v-next/src/modules/programmatic-personalization/ui/DynamicProductRecommendations/DynamicProductRecommendations.tsx
+++ apps/csk-v-next/src/modules/programmatic-personalization/ui/DynamicProductRecommendations/DynamicProductRecommendations.tsx
@@ -3,9 +3,9 @@
 import { FC, useEffect, useState } from 'react';
 import { useScores } from '@uniformdev/canvas-next-rsc-client';
 import { Flex as CSKFlex, Grid } from '@uniformdev/csk-components/components/ui';
-import { cn } from '@uniformdev/csk-components/utils/styling';
 import { ProductCard } from '@/components/custom-ui/ProductCard';
 import { Product } from '@/modules/cart/types';
+import { ProductCardSkeleton } from '@/modules/favorites';
 import { getEnrichmentAndFieldKey, getMaxEnrichmentKey } from '@/modules/programmatic-personalization';
 import { DynamicRecommendationsProps } from '.';
 import { ProductBoostEnrichment } from '../../types';
@@ -19,7 +19,6 @@ const DynamicProductRecommendations: FC<DynamicRecommendationsProps> = ({
   fluidContent,
   fullHeight,
   border,
-  loadingIndicatorColor,
   boostEnrichments,
   maxRecommendations = '3',
 }) => {
@@ -111,44 +110,27 @@ const DynamicProductRecommendations: FC<DynamicRecommendationsProps> = ({
       {...{ backgroundColor, spacing, border, fluidContent, fullHeight }}
     >
       {title}
-      {isLoading ? (
-        <div className="text-center">
-          <svg
-            className={cn('inline size-16 animate-spin', {
-              [`text-${loadingIndicatorColor}`]: loadingIndicatorColor,
+
+      <Grid columnsCount="3" gapY="8" gapX="8">
+        {isLoading
+          ? Array.from({ length: parseInt(maxRecommendations) }, () => 0).map((_, index) => (
+              <ProductCardSkeleton key={index} />
+            ))
+          : products.map(product => {
+              const { price = 0, currency = '$' } = product?.variants?.[0] ?? {};
+              return (
+                <ProductCard
+                  key={product.slug}
+                  price={`${currency}${price}`}
+                  title={product.title}
+                  image={product.primaryImage?.[0]?.url}
+                  slug={product.slug}
+                  link={`/products/${product.slug}`}
+                  textColor={'primary'}
+                />
+              );
             })}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-          <h2 className="mt-4 text-xl font-semibold text-gray-700">Loading awesome stuff...</h2>
-          <p className="mt-2 text-gray-500">Get ready for some great recommendations!</p>
-        </div>
-      ) : (
-        <Grid columnsCount="3" gapY="8" gapX="8">
-          {products.map(product => {
-            const { price = 0, currency = '$' } = product?.variants?.[0] ?? {};
-            return (
-              <ProductCard
-                key={product.slug}
-                price={`${currency}${price}`}
-                title={product.title}
-                image={product.primaryImage?.[0]?.url}
-                slug={product.slug}
-                link={`/products/${product.slug}`}
-                textColor={'primary'}
-              />
-            );
-          })}
-        </Grid>
-      )}
+      </Grid>
     </CSKFlex>
   );
 };
