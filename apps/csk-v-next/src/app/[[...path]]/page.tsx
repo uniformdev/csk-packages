d70d2b747a798a4c6d3f59225516226d21ad4be8
trsergey <stu@dbbsoftware.com>
2025-04-08 15:25:08 +0300

    feat: deviceTypeSetter (server-side)

--- apps/csk-v-next/src/app/[[...path]]/page.tsx
+++ apps/csk-v-next/src/app/[[...path]]/page.tsx
@@ -1,29 +1,41 @@
+import { cookies } from 'next/headers';
 import {
   PageParameters,
   UniformComposition,
+  createServerUniformContext,
   isIncontextEditingEnabled,
   retrieveRoute,
+  ContextUpdateTransfer,
 } from '@uniformdev/canvas-next-rsc';
 import { emptyPlaceholderResolver } from '@uniformdev/csk-components/components/canvas/emptyPlaceholders';
 import { DesignExtensionsProvider } from '@uniformdev/design-extensions-tools/components/providers/server';
 import { componentResolver } from '@/components';
-import { DeviceTypeSetter } from '@/components/custom-ui/DeviceTypeSetter';
 
 export default async function Home(props: PageParameters) {
+  const cookieStore = await cookies();
+  const deviceType = cookieStore.get('deviceType')?.value || '';
   const route = await retrieveRoute(props);
-
   const searchParams = await props.searchParams;
+  const serverContext = await createServerUniformContext({
+    searchParams,
+  });
   const isPreviewMode = isIncontextEditingEnabled({ searchParams });
   return (
     <DesignExtensionsProvider isPreviewMode={isPreviewMode}>
+      <ContextUpdateTransfer
+        serverContext={serverContext}
+        update={{
+          quirks: { ...(deviceType ? { deviceType } : undefined) },
+        }}
+      />
       <UniformComposition
         {...props}
         route={route}
         resolveComponent={componentResolver}
         mode="server"
         resolveEmptyPlaceholder={emptyPlaceholderResolver}
+        serverContext={serverContext}
       />
-      <DeviceTypeSetter />
     </DesignExtensionsProvider>
   );
 }
