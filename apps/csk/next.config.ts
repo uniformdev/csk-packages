e66daca2be5ac2c92191de4a4f342bae9382a4b1
Eugene <eugene.shulga@dbbsoftware.com>
2025-05-15 17:21:35 +0300

    feat: baseline-recipes

--- apps/csk/next.config.ts
+++ apps/csk/next.config.ts
@@ -1,6 +1,11 @@
 import { NextConfig } from 'next';
+//? if (localization) {
+import createNextIntlPlugin from 'next-intl/plugin';
+//? }
 import { withUniformConfig } from '@uniformdev/canvas-next-rsc/config';
-
+//? if (localization) {
+const withNextIntl = createNextIntlPlugin();
+//? }
 /** @type {NextConfig} */
 const nextConfig: NextConfig = {
   images: {
@@ -8,5 +13,9 @@ const nextConfig: NextConfig = {
     deviceSizes: [320, 420, 640, 768, 1024, 1280, 1536],
   },
 };
-
-export default withUniformConfig(nextConfig);
+//? if (localization) {
+export default withNextIntl(withUniformConfig(nextConfig));
+//? }
+//? if (!localization) {
+//? write('export default withUniformConfig(nextConfig);\n');
+//? }
