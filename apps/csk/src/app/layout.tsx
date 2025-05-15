0fbc6ac3b396c69d5d6ed32780676eb0d99b4a96
Eugene <eugene.shulga@dbbsoftware.com>
2025-05-15 17:21:35 +0300

    feat: baseline-recipes

--- apps/csk/src/app/layout.tsx
+++ apps/csk/src/app/layout.tsx
@@ -1,4 +1,7 @@
 import { ReactNode } from 'react';
+//? if (localization) {
+import { NextIntlClientProvider } from 'next-intl';
+//? }
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import { UniformContext } from '@uniformdev/canvas-next-rsc';
 import '@/styles/globals.css';
@@ -7,15 +10,28 @@ import '@/styles/dimensions.css';
 import '@/styles/fonts.css';
 import '@/styles/borders.css';
 import { customFontVariables } from '@/fonts';
+import { UniformClientContext } from '@/utils/clientContext';
+//? if (ga) {
+import { GoogleAnalytics } from '@next/third-parties/google';
+//? }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={customFontVariables}>
-        <NextThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <UniformContext>{children}</UniformContext>
-        </NextThemeProvider>
+        {/* //? if (localization) { */}
+        <NextIntlClientProvider>
+          {/* //? } */}
+          <NextThemeProvider attribute="class" defaultTheme="light" enableSystem>
+            <UniformContext clientContextComponent={UniformClientContext}>{children}</UniformContext>
+          </NextThemeProvider>
+          {/* //? if (localization) { */}
+        </NextIntlClientProvider>
+        {/* //? } */}
       </body>
+      {/* //? if (ga) { */}
+      {process.env.GOOGLE_ANALYTICS_ID && <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />}
+      {/* //? } */}
     </html>
   );
 }
