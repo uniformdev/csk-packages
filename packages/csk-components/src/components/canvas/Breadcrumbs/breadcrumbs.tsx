1c43414352a7e33177b1bfb4868b0c76d9b335dc
eugeneshulga869 <82021957+eugeneshulga869@users.noreply.github.com>
2025-06-04 11:44:34 +0300

    feat: add possibility to auto generate breadcrumbs (#127)

--- packages/csk-components/src/components/canvas/Breadcrumbs/breadcrumbs.tsx
+++ packages/csk-components/src/components/canvas/Breadcrumbs/breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
-import { flattenValues, LinkParamValue } from '@uniformdev/canvas';
+import { flattenValues, LinkParamValue, CanvasClient } from '@uniformdev/canvas';
+import { ProjectMapClient } from '@uniformdev/project-map';
 import BaseButton, { ButtonVariant } from '@/components/ui/Button';
 import BaseText from '@/components/ui/Text';
 import { formatUniformLink } from '@/utils/routing';
@@ -7,12 +8,71 @@ import { cn } from '@/utils/styling';
 import { BreadcrumbLink, BreadcrumbsProps } from '.';
 import { getSeparator } from './helpers';
 
-export const Breadcrumbs: FC<BreadcrumbsProps> = ({ separator, links, size, color, font, transform }) => {
-  const itemToDisplay =
-    links?.reduce<BreadcrumbLink[]>((acc, linkItem) => {
-      const { title, link } = (flattenValues(linkItem) as { title?: string; link?: LinkParamValue } | undefined) || {};
-      return title ? [...acc, { title, link: formatUniformLink(link) }] : acc;
+export const Breadcrumbs: FC<BreadcrumbsProps> = async ({
+  separator,
+  links,
+  size,
+  color,
+  font,
+  transform,
+  autoGenerate,
+  context,
+}) => {
+  const getManualBreadcrumbs = async (): Promise<BreadcrumbLink[]> =>
+    links?.reduce<BreadcrumbLink[]>((acc, item) => {
+      const { title, link } = flattenValues(item) as {
+        title?: string;
+        link?: LinkParamValue;
+      };
+
+      if (!title) return acc;
+
+      return [...acc, { title, link: formatUniformLink(link) }];
     }, []) || [];
+
+  const getAutoBreadcrumbs = async (): Promise<BreadcrumbLink[]> => {
+    const client = new ProjectMapClient({
+      projectId: process.env.UNIFORM_PROJECT_ID,
+      apiKey: process.env.UNIFORM_API_KEY,
+    });
+
+    const { nodes } = await client.getNodes({
+      path: context?.matchedRoute,
+      includeAncestors: true,
+    });
+
+    if (!nodes?.length) return [];
+
+    return Promise.all(
+      nodes.map(async node => {
+        const isDynamic = node.pathSegment?.includes(':');
+
+        const title =
+          isDynamic && node.compositionId
+            ? await new CanvasClient({
+                projectId: process.env.UNIFORM_PROJECT_ID,
+                apiKey: process.env.UNIFORM_API_KEY,
+              })
+                .getCompositionById({ compositionId: node.compositionId })
+                .then(({ composition }) => {
+                  if (!composition) return node.name;
+                  const flattened = flattenValues(composition);
+                  return (flattened?.pageTitle as string) || node.name;
+                })
+                .catch(() => node.name)
+            : node.name;
+
+        const link =
+          node.type === 'placeholder'
+            ? undefined
+            : node.path.replace(/:([a-zA-Z0-9-_]+)/g, (_, key) => context?.dynamicInputs?.[key] ?? `:${key}`);
+
+        return { title, link };
+      })
+    );
+  };
+
+  const itemToDisplay = autoGenerate ? await getAutoBreadcrumbs() : await getManualBreadcrumbs();
 
   if (!itemToDisplay.length) {
     // ToDo: Add breadcrumbs placeholder
