0f4c44dfc697ba147399198f6549ca433d4eccff
trsergey <stu@dbbsoftware.com>
2025-06-19 13:44:57 +0300

    chore: allow all robots to access the site

--- apps/csk-marketing-site/src/app/robots.ts
+++ apps/csk-marketing-site/src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      disallow: '/',
+      allow: '/',
     },
     sitemap: `${domain}/sitemap.xml`,
   };
