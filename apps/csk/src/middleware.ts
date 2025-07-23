47b2ea25c9ffde32e5f3b14476268c55efe8cb29
Eugene <eugene.shulga@dbbsoftware.com>
2025-07-23 15:05:04 +0300

    feat: baseline-recipes

--- apps/csk/src/middleware.ts
+++ apps/csk/src/middleware.ts
@@ -1,10 +1,30 @@
+//? if (localization) {
 import { NextRequest } from 'next/server';
+import createIntlMiddleware from 'next-intl/middleware';
+//? } else {
+//? write('import { NextRequest, NextResponse } from "next/server";\n');
+//? }
 import { uniformMiddleware } from '@uniformdev/canvas-next-rsc-v2/middleware';
 import locales from '@/i18n/locales.json';
+//? if (localization) {
+import { routing } from './i18n/routing';
+//? }
 import { formatPath } from './utils/formatPath';
 
+//? if (localization) {
+const intlMiddleware = createIntlMiddleware(routing);
+//? }
+
 export async function middleware(request: NextRequest) {
-  return uniformMiddleware({
+  //? if (localization) {
+  const baseResponse = intlMiddleware(request);
+  //? } else {
+  //? write('const baseResponse = NextResponse.next();\n');
+  //? }
+
+  if (!baseResponse.ok) return baseResponse;
+
+  const response = await uniformMiddleware({
     rewriteRequestPath: async ({ url }) => ({ path: formatPath(url.pathname, locales.defaultLocale) }),
   })(request).then(result =>
     result.headers.get('x-middleware-rewrite')
@@ -13,6 +33,12 @@ export async function middleware(request: NextRequest) {
           rewriteRequestPath: async ({ url }) => ({ path: formatPath(url.pathname, undefined) }),
         })(request)
   );
+
+  baseResponse.cookies.getAll().forEach(cookie => {
+    response.cookies.set(cookie);
+  });
+
+  return response;
 }
 
 export const config = {
