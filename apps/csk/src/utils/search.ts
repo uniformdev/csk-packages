64f4e215ee4e3e9fc2b882cc3704c030036684c3
trsergey <stu@dbbsoftware.com>
2025-09-03 12:08:00 +0300

    fix: add enrichment boosted order functionality

--- apps/csk/src/utils/search.ts
+++ apps/csk/src/utils/search.ts
@@ -1,41 +1,38 @@
-import { OrderBy } from '../types';
+import { ScoreVector } from '@uniformdev/context';
+import { BoostInclusions, OrderBy } from '../types';
 
 export const buildOrderByQuery = (orderBy: OrderBy) => {
   return `${orderBy.field}_${orderBy.direction}`;
 };
 
-export const getMaxEnrichmentKey = (boostEnrichment: string, scores: Record<string, number>): string | null => {
-  return Object.entries(scores).reduce<{ key: string | null; score: number }>(
-    (acc, [key, score]) => {
-      if (key.startsWith(boostEnrichment)) {
-        return score > acc.score ? { key, score } : acc;
-      }
-
-      return acc;
-    },
-    { key: null, score: -Infinity }
-  ).key;
+export const getEnrichmentKeysWithScore = (boostEnrichment: string, scores: ScoreVector): string[] => {
+  return Object.entries(scores)
+    .filter(([key, score]) => key.startsWith(boostEnrichment) && score > 0)
+    .map(([key]) => key);
 };
 
-export const getOrderByClause = (userType: Record<string, string>) => {
-  if (!userType) {
+export const getOrderByClause = (userType: BoostInclusions, maxBoosts: number) => {
+  if (!Object.keys(userType).length) {
     return undefined;
   }
-
-  const orderFields = Object.entries(userType).map(([category, value]) => {
-    const { fieldKey } = getEnrichmentAndFieldKey(category);
-
-    return `fields.${fieldKey}.slug:${value}:3`;
-  });
+  const orderFields = Object.entries(userType)
+    .map(([category, data]) => {
+      const { fieldKey } = getEnrichmentAndFieldKey(category);
+      if (!fieldKey) {
+        return '';
+      }
+      return `fields.${fieldKey}.slug:${data.value}:${data.score}`;
+    })
+    .filter(Boolean);
 
   if (orderFields.length === 0) {
     return undefined;
   }
 
-  return `boost|${orderFields.join('|')}`;
+  return `boost|${orderFields.slice(0, maxBoosts).join('|')}`;
 };
 
 export const getEnrichmentAndFieldKey = (enrichment: string) => {
-  const [enrichmentKey, fieldKey] = enrichment.split(',');
-  return { enrichmentKey, fieldKey };
+  const [enrichmentKey = '', fieldKey = ''] = enrichment.split(',') || [];
+  return { enrichmentKey: enrichmentKey.trim(), fieldKey: fieldKey.trim() };
 };
