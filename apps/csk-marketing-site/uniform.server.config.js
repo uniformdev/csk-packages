fb58cef81feef422768f34c3d8c294cbdb6a0efb
trsergey25 <102293330+trsergey25@users.noreply.github.com>
2025-09-08 13:45:18 +0300

    chore: update uniform config for csk and csk-marketing-site (#209)
    * chore: update uniform config
    
    * chore: generate static pages from project map

--- apps/csk-marketing-site/uniform.server.config.js
+++ apps/csk-marketing-site/uniform.server.config.js
@@ -1,9 +1,6 @@
 /** @type {import('@uniformdev/canvas-next-rsc-v2/config').UniformServerConfig} */
 module.exports = {
   defaultConsent: true,
-  evaluation: {
-    personalization: 'hybrid',
-  },
   playgroundPath: '/playground',
   experimental: {
     middlewareRuntimeCache: true,
