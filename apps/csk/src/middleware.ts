3b275dc97375a123d4ad1c0c2af729f1c5393f87
trsergey <stu@dbbsoftware.com>
2025-09-16 11:18:19 +0300

    chore: remove Node.js runtime specification

--- apps/csk/src/middleware.ts
+++ apps/csk/src/middleware.ts
@@ -18,5 +18,3 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
-
-export const runtime = 'nodejs';
