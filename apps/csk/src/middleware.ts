1ab862e23e9e9ef50e43772a37f84585fd39abce
Alex Shyba <alex.shyba@uniform.dev>
2025-09-17 11:06:46 -0700

    test: basic middleware

--- apps/csk/src/middleware.ts
+++ apps/csk/src/middleware.ts
@@ -1,51 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { uniformMiddleware } from '@uniformdev/canvas-next-rsc-v2/middleware';
-import locales from '@/i18n/locales.json';
-import { DEVICE_TYPE_COOKIE_NAME, getDeviceType } from './utils/deviceType';
-import { formatPath } from './utils/formatPath';
 
-export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/playground') {
-    return uniformMiddleware()(request);
-  }
-
-  const baseResponse = NextResponse.next();
-
-  if (!baseResponse.ok) return baseResponse;
-
-  const response = await uniformMiddleware({
-    rewriteRequestPath: async ({ url }) => ({
-      path: formatPath(url.pathname, locales.defaultLocale),
-      keys: {
-        search: url.toString(),
-      },
-    }),
-  })(request).then(result =>
-    result.headers.get('x-middleware-rewrite')
-      ? result
-      : uniformMiddleware({
-          rewriteRequestPath: async ({ url }) => ({
-            path: formatPath(url.pathname, undefined),
-            keys: {
-              search: url.toString(),
-            },
-          }),
-        })(request)
-  );
-
-  baseResponse.cookies.getAll().forEach(cookie => {
-    response.cookies.set(cookie);
-  });
-
-  if (!request.cookies.get(DEVICE_TYPE_COOKIE_NAME)) {
-    response.cookies.set(DEVICE_TYPE_COOKIE_NAME, getDeviceType(request.headers.get('user-agent')));
-  }
-
-  return response;
-}
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
-};
+export default uniformMiddleware();
 
 export const runtime = 'nodejs';
