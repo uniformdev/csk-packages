38f266b51f5ed3024450daf4b3cf1e2256309f89
Alex Shyba <alex.shyba@uniform.dev>
2025-09-17 10:42:24 -0700

    fix: nodejs runtime

--- apps/csk/src/middleware.ts
+++ apps/csk/src/middleware.ts
@@ -47,3 +47,5 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
+
+export const runtime = 'nodejs';
