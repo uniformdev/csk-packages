f52fab8081c92cd108bf46b5fea5e7acd8642a0b
trsergey25 <102293330+trsergey25@users.noreply.github.com>
2025-11-25 17:44:21 +0200

    chore: refactor CSK content structure (#226)

--- apps/csk/uniform.config.ts
+++ apps/csk/uniform.config.ts
@@ -3,7 +3,7 @@ import { uniformConfig } from '@uniformdev/cli/config';
 module.exports = uniformConfig({
   preset: 'all',
   overrides: {
-    serializationConfig: { directory: './content' },
+    serializationConfig: { directory: './content/full' },
   },
   // you can disable specific entities from being included, which can speed up your sync
   disableEntities: ['webhook'],
