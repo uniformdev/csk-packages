96bd8aab373254a165d6f1de2a1e70b827e70848
trsergey <stu@dbbsoftware.com>
2025-12-05 13:54:40 +0200

    chore: switche to middleware-based composition

--- apps/csk/uniform.server.config.js
+++ apps/csk/uniform.server.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@uniformdev/canvas-next-rsc-v2/config').UniformServerConfig} */
 module.exports = {
+  defaultConsent: true,
   playgroundPath: '/playground',
   experimental: {
     middlewareRuntimeCache: true,
