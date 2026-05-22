251b4d2012c2b16e7996b1382ea3dfed0b288b62
eugeneshulga869 <82021957+eugeneshulga869@users.noreply.github.com>
2026-05-22 15:44:10 +0300

    feat: wrap dynamic token fetching in Suspense (#259)

--- packages/design-extensions-tools/src/components/providers/DesignExtensionsProvider/design-extensions-provider.tsx
+++ packages/design-extensions-tools/src/components/providers/DesignExtensionsProvider/design-extensions-provider.tsx
@@ -1,25 +1,30 @@
 /* eslint-disable react-hooks/error-boundaries */
-import { FC } from 'react';
+import { FC, PropsWithChildren, Suspense } from 'react';
 import { DesignExtensionsProviderProps } from '.';
 import { getTokenConfiguration } from '../../../scripts/run-time';
 
-export const DesignExtensionsProvider: FC<DesignExtensionsProviderProps> = async ({
-  children,
-  isPreviewMode = false,
-}) => {
+const WithStylesWrapper: FC<PropsWithChildren> = async ({ children }) => {
+  const { colors, dimensions, defaultFont, borders } = (await getTokenConfiguration()) || {};
+  return (
+    <div className={defaultFont ? `font-${defaultFont}` : ''}>
+      {!!colors && <div dangerouslySetInnerHTML={{ __html: colors }} />}
+      {!!dimensions && <div dangerouslySetInnerHTML={{ __html: dimensions }} />}
+      {!!borders && <div dangerouslySetInnerHTML={{ __html: borders }} />}
+      {children}
+    </div>
+  );
+};
+
+export const DesignExtensionsProvider: FC<DesignExtensionsProviderProps> = ({ children, isPreviewMode = false }) => {
   try {
     if (!isPreviewMode && process.env.WATCH !== 'true') {
       return <div className="font-default">{children}</div>;
     }
 
-    const { colors, dimensions, defaultFont, borders } = (await getTokenConfiguration()) || {};
     return (
-      <div className={defaultFont ? `font-${defaultFont}` : ''}>
-        {!!colors && <div dangerouslySetInnerHTML={{ __html: colors }} />}
-        {!!dimensions && <div dangerouslySetInnerHTML={{ __html: dimensions }} />}
-        {!!borders && <div dangerouslySetInnerHTML={{ __html: borders }} />}
-        {children}
-      </div>
+      <Suspense fallback={<div className="font-default">{children}</div>}>
+        <WithStylesWrapper>{children}</WithStylesWrapper>
+      </Suspense>
     );
   } catch (e) {
     console.error(e);
